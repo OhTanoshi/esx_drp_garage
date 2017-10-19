22a891a321f29752006c57833a6c5a8abffd7672
TanguyOrtegat <tanguy.ortegat@student.odisee.be>
2017-10-19 02:32:30 +0200

    Update eden_garage.sql

--- eden_garage.sql
+++ eden_garage.sql
@@ -1,1 +1,7 @@
-ALTER TABLE `owned_vehicles` ADD `state` BOOLEAN NOT NULL DEFAULT FALSE COMMENT 'Etat de la voiture' AFTER `owner`;
+CREATE TABLE `owned_vehicles` (
+  `id` int(11) NOT NULL,
+  `vehicle` longtext NOT NULL,
+  `owner` varchar(60) NOT NULL,
+  `state` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Etat de la voiture',
+  `value` int(11) NOT NULL DEFAULT '0
+);
